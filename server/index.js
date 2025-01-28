faaf34ef09a2d7ea18de95ebc7eb75520fbd75f6
Rajthegreat123 <rajshekharan2020@gmail.com>
2025-01-28 00:08:59 +0000

    Fix

--- server/index.js
+++ server/index.js
@@ -357,7 +357,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
